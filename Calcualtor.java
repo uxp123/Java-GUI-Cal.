69bf283f1342794e2f86bb0126e02f57a6d3b258
Papsy <57874831+uxp123@users.noreply.github.com>
2020-08-15 19:27:40 +0000

    Add files via upload

---
+++ Calcualtor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcualtor;
+
+/**
+ *
+ * @author philipfynn
+ */
+public class Calcualtor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = 0;
+        
+        sum = a + b;
+        System.out.println("");
+        // TODO code application logic here
+    }
+    
+}
