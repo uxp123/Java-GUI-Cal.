4de05421035daa6a94d68247e47add7ebe4a3d1d
Papsy <57874831+uxp123@users.noreply.github.com>
2020-08-15 19:28:26 +0000

    Update and rename Calcualtor.java to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package calcualtor;
 
-/**
- *
- * @author philipfynn
- */
-public class Calcualtor {
+public class Calculator {
 
     /**
      * @param args the command line arguments
