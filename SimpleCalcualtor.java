647974a421a80e80ada3eb6817fb88d7e668df44
Papsy <57874831+uxp123@users.noreply.github.com>
2020-08-15 19:29:05 +0000

    Update SimpleCalcualtor.java

--- SimpleCalcualtor.java
+++ SimpleCalcualtor.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package calcualtor;
 
-/**
- *
- * @author philipfynn
- */
 public class SimpleCalcualtor extends javax.swing.JFrame {
 
     int firstnum;
